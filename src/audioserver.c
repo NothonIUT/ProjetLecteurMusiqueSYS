34236f627fcb51822d9ee0f4bc49bebaa5bf4407
NothonIUT <lemoannathan@gmail.com>
2021-03-16 16:12:49 +0100

    Ajout de la récupération de l'adresse du client dans le serveur

--- src/audioserver.c
+++ src/audioserver.c
@@ -10,11 +10,15 @@
 
 int main(){
 
-    // Création du socket UDP sur le port 1234
-    int socket_descriptor, err_bind;
-    struct sockaddr_in address;
+    // Variables
+    int socket_descriptor, err_bind; // Descripteur du socket et indicateur du succès (ou non) de bind
+    struct sockaddr_in address, from; // Structures contenant l'addresse du socket et l'addresse de la source des messages
+    char msg[64]; // Buffer pour le message
+    socklen_t rlen, flen; // Variables pour le nombre d'octets reçus et pour la longueur de l'addresse source des messages
 
-    socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0); // Création du Socket
+
+    // Créatoion du Socket UDP sur le port 1234
+    socket_descriptor = socket(AF_INET, SOCK_DGRAM, 0); 
     // Traitement des erreurs
     if (socket_descriptor < 0){
         // A completer
@@ -31,4 +35,16 @@ int main(){
     }
 
     // TODO : Envoie des données de la musique (Choix de la musique, lecture du fichier, envoie des données)
+
+    // Recevoir un message du client afin de récupérer son IP
+    flen = sizeof(struct sockaddr_in);
+    rlen = recvfrom(socket, msg, sizeof(msg), 0, (struct sockaddr*) &from, &flen); // On attend de recevoir un message
+
+    // Traitement des erreurs
+    if(rlen < 0){
+        // A compléter
+    }
+
+    // TODO : Communication avec le client
+
 }
