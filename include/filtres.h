3f2c64926812c9945341cfa56019f4bfcd7f5a9c
Nathan <lemoannathan@gmail.com>
2021-03-29 15:18:00 +0200

    Ajout du filtre d'accélération/ralentissement #2

--- include/filtres.h
+++ include/filtres.h
@@ -1,4 +1,10 @@
-/*
-*
-*
+// filtres.[ch]
+// Déclaration des fonctions d'application des filtres 
+
+/* Modifie le sample_rate afin d'accélerer ou ralentir la lecture de la musique.
+*  La fonction renvoie -1 si le sample_rate est invalide (i.e s'il est négatif ou nul)
+*  La fonction renvoie -2 si le facteur d'accélération est invalide (i.e si factor est inférieur à 0.3)
+*  Cette limite a été choisie afin de ne pas trop allonger la durée de la musique jouée. 
+*  La fonction renvoie le produit de sample_rate et factor sinon.
 */
+int modify_song_speed(int factor, int sample_rate);
--- src/filtres.c
+++ src/filtres.c
@@ -5,3 +5,18 @@
 #include <string.h>
 #include "../include/audio.h"
 #include "../include/filtres.h"
+
+int modify_song_speed(int factor, int sample_rate){
+    
+    // Vérification de la valeur de sample_rate
+    if(sample_rate < 0){
+        return -1;
+    }
+    
+    // Vérification de la valeur de factor
+    if(factor < 0.3){
+        return -2;
+    }
+
+    return sample_rate*factor;
+}
