452bc58c1b1ca1e3066ee2e41da960b3709ff977
Nathan <lemoannathan@gmail.com>
2021-03-29 14:34:36 +0200

    Début de la conception des filtres

---
+++ src/filtres.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/soundcard.h>
+#include <unistd.h>
+#include <string.h>
+#include "../include/audio.h"
+
+int main(){
+
+    /* idées de filtres
+    *
+    */
+
+
+   return 1;
+}
